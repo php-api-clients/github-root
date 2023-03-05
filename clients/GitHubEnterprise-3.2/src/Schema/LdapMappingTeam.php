40ce79acac9e8ac789374b56120dc48970d42946
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-03-05 21:37:57 +0100

    Only use string for example data when type is actually a string

--- clients/GitHubEnterprise-3.2/src/Schema/LdapMappingTeam.php
+++ clients/GitHubEnterprise-3.2/src/Schema/LdapMappingTeam.php
@@ -12,7 +12,7 @@ final readonly class LdapMappingTeam
     public const SCHEMA_JSON = '{"type":"object","properties":{"ldap_dn":{"type":"string"},"id":{"type":"integer"},"node_id":{"type":"string"},"url":{"type":"string"},"html_url":{"type":"string"},"name":{"type":"string"},"slug":{"type":"string"},"description":{"type":["string","null"]},"privacy":{"type":"string"},"permission":{"type":"string"},"members_url":{"type":"string"},"repositories_url":{"type":"string"},"parent":{"type":["null"]}}}';
     public const SCHEMA_TITLE = '';
     public const SCHEMA_DESCRIPTION = '';
-    public const SCHEMA_EXAMPLE_DATA = '{"ldap_dn":"generated_ldap_dn","id":13,"node_id":"generated_node_id","url":"generated_url","html_url":"generated_html_url","name":"generated_name","slug":"generated_slug","description":"generated_description","privacy":"generated_privacy","permission":"generated_permission","members_url":"generated_members_url","repositories_url":"generated_repositories_url","parent":"generated_parent"}';
+    public const SCHEMA_EXAMPLE_DATA = '{"ldap_dn":"generated_ldap_dn","id":13,"node_id":"generated_node_id","url":"generated_url","html_url":"generated_html_url","name":"generated_name","slug":"generated_slug","description":"generated_description","privacy":"generated_privacy","permission":"generated_permission","members_url":"generated_members_url","repositories_url":"generated_repositories_url","parent":null}';
     public ?string $ldap_dn;
     public ?int $id;
     public ?string $node_id;
