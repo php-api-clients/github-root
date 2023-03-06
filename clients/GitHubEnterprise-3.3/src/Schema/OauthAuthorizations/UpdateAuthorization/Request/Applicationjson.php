ad06e0f103455aef84963f5c53f3f52d8477c952
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-03-06 23:11:41 +0100

    Always take the first example
    By doing this we'll prevent generation churn with only example updates

--- clients/GitHubEnterprise-3.3/src/Schema/OauthAuthorizations/UpdateAuthorization/Request/Applicationjson.php
+++ clients/GitHubEnterprise-3.3/src/Schema/OauthAuthorizations/UpdateAuthorization/Request/Applicationjson.php
@@ -12,7 +12,7 @@ final readonly class Applicationjson
     public const SCHEMA_JSON = '{"type":"object","properties":{"scopes":{"type":["array","null"],"items":{"type":"string"},"description":"A list of scopes that this authorization is in.","examples":["public_repo","user"]},"add_scopes":{"type":"array","items":{"type":"string"},"description":"A list of scopes to add to this authorization."},"remove_scopes":{"type":"array","items":{"type":"string"},"description":"A list of scopes to remove from this authorization."},"note":{"type":"string","description":"A note to remind you what the OAuth token is for.","examples":["Update all gems"]},"note_url":{"type":"string","description":"A URL to remind you what app the OAuth token is for."},"fingerprint":{"type":"string","description":"A unique string to distinguish an authorization from others created for the same client ID and user."}}}';
     public const SCHEMA_TITLE = '';
     public const SCHEMA_DESCRIPTION = '';
-    public const SCHEMA_EXAMPLE_DATA = '{"scopes":["user"],"add_scopes":["generated_add_scopes"],"remove_scopes":["generated_remove_scopes"],"note":"Update all gems","note_url":"generated_note_url","fingerprint":"generated_fingerprint"}';
+    public const SCHEMA_EXAMPLE_DATA = '{"scopes":["public_repo"],"add_scopes":["generated_add_scopes"],"remove_scopes":["generated_remove_scopes"],"note":"Update all gems","note_url":"generated_note_url","fingerprint":"generated_fingerprint"}';
     /**
      * scopes: A list of scopes that this authorization is in.
      * @param ?array<string> $scopes
