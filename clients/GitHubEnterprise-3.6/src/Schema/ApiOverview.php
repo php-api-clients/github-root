44c7b56c91591dcee17f3ebedb22385a9fed2b07
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-03-06 22:57:04 +0100

    Updates

--- clients/GitHubEnterprise-3.6/src/Schema/ApiOverview.php
+++ clients/GitHubEnterprise-3.6/src/Schema/ApiOverview.php
@@ -12,7 +12,7 @@ final readonly class ApiOverview
     public const SCHEMA_JSON = '{"title":"Api Overview","required":["verifiable_password_authentication"],"type":"object","properties":{"verifiable_password_authentication":{"type":"boolean","examples":[true]},"packages":{"type":"array","items":{"type":"string"},"examples":["13.65.0.0\\/16","157.55.204.33\\/32","2a01:111:f403:f90c::\\/62"]},"dependabot":{"type":"array","items":{"type":"string"},"examples":["192.168.7.15\\/32","192.168.7.16\\/32"]},"installed_version":{"type":"string","examples":["3.6.0"]}},"description":"Api Overview"}';
     public const SCHEMA_TITLE = 'Api Overview';
     public const SCHEMA_DESCRIPTION = 'Api Overview';
-    public const SCHEMA_EXAMPLE_DATA = '{"verifiable_password_authentication":true,"packages":["13.65.0.0\\/16"],"dependabot":["192.168.7.16\\/32"],"installed_version":"3.6.0"}';
+    public const SCHEMA_EXAMPLE_DATA = '{"verifiable_password_authentication":true,"packages":["157.55.204.33\\/32"],"dependabot":["192.168.7.16\\/32"],"installed_version":"3.6.0"}';
     /**
      * @param array<string> $packages
      * @param array<string> $dependabot
