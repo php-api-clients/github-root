1c8d986eafb0f7b4bb52fbcdaba417e558cd6dec
WyriHaximus <WyriHaximus@users.noreply.github.com>
2023-06-01 02:29:33 +0000

    [GitHubEnterprise-3.0] Update to 1.1.4 (hash: 3e0a7810d254da4d26f0002e587eb763current=3e0a7810d254da4d26f0002e587eb763) from 1.1.4 (hash: ) Detected Schema changes:

--- clients/GitHubEnterprise-3.0/src/Schema/ContentDirectory.php
+++ clients/GitHubEnterprise-3.0/src/Schema/ContentDirectory.php
@@ -5,16 +5,7 @@ declare(strict_types=1);
 namespace ApiClients\Client\GitHubEnterprise\Schema;
 
 use ApiClients\Client\GitHubEnterprise\Schema;
-use EventSauce\ObjectHydrator\MapFrom;
 
-final readonly class ContentDirectory
+final readonly class ContentDirectory extends Schema\AliasAbstract\Tiet60960B4E\TietDF03118C\TietE1512B4C\TietEC63F6C4
 {
-    public const SCHEMA_JSON         = '{"required":["_links","git_url","html_url","download_url","name","path","sha","size","type","url"],"type":"object","properties":{"type":{"type":"string"},"size":{"type":"integer"},"name":{"type":"string"},"path":{"type":"string"},"content":{"type":"string"},"sha":{"type":"string"},"url":{"type":"string","format":"uri"},"git_url":{"type":["string","null"],"format":"uri"},"html_url":{"type":["string","null"],"format":"uri"},"download_url":{"type":["string","null"],"format":"uri"},"_links":{"required":["git","html","self"],"type":"object","properties":{"git":{"type":["string","null"],"format":"uri"},"html":{"type":["string","null"],"format":"uri"},"self":{"type":"string","format":"uri"}}}}}';
-    public const SCHEMA_TITLE        = '';
-    public const SCHEMA_DESCRIPTION  = '';
-    public const SCHEMA_EXAMPLE_DATA = '{"type":"generated","size":4,"name":"generated","path":"generated","content":"generated","sha":"generated","url":"https:\\/\\/example.com\\/","git_url":"https:\\/\\/example.com\\/","html_url":"https:\\/\\/example.com\\/","download_url":"https:\\/\\/example.com\\/","_links":{"git":"https:\\/\\/example.com\\/","html":"https:\\/\\/example.com\\/","self":"https:\\/\\/example.com\\/"}}';
-
-    public function __construct(public string $type, public int $size, public string $name, public string $path, public ?string $content, public string $sha, public string $url, #[MapFrom('git_url')] public ?string $gitUrl, #[MapFrom('html_url')] public ?string $htmlUrl, #[MapFrom('download_url')] public ?string $downloadUrl, #[MapFrom('_links')] public Schema\ContentDirectory\Links $links)
-    {
-    }
 }
--- clients/GitHubEnterprise-3.0/src/Schema/Gists/Update/Request/ApplicationJson.php
+++ clients/GitHubEnterprise-3.0/src/Schema/Gists/Update/Request/ApplicationJson.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace ApiClients\Client\GitHubEnterprise\Schema\Gists\Update\Request;
 
+use ApiClients\Client\GitHubEnterprise\Schema;
+
 final readonly class ApplicationJson
 {
     public const SCHEMA_JSON         = '{"type":["object","null"],"anyOf":[{"required":["description"]},{"required":["files"]}],"properties":{"description":{"type":"string","description":"Description of the gist","examples":["Example Ruby script"]},"files":{"type":"object","description":"Names of files to be updated","additionalProperties":{"type":["object","null"],"anyOf":[{"required":["content"]},{"required":["filename"]},{"maxProperties":0,"type":"object"}],"properties":{"content":{"type":"string","description":"The new content of the file"},"filename":{"type":["string","null"],"description":"The new filename for the file"}}},"examples":[{"hello.rb":{"content":"blah","filename":"goodbye.rb"}}]}}}';
@@ -15,7 +17,7 @@ final readonly class ApplicationJson
      * description: Description of the gist
      * files: Names of files to be updated
      */
-    public function __construct(public ?string $description, public string $files)
+    public function __construct(public ?string $description, public ?Schema\Gists\Update\Request\ApplicationJson\Files $files)
     {
     }
 }
