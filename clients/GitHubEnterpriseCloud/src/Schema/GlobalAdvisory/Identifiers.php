8f78d936b0530fffa1f5b493d82ff2e34c18675c
WyriHaximus <WyriHaximus@users.noreply.github.com>
2023-07-29 13:00:40 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4 (hash: 8a071f341e7a01c0626fe0ebf3a81441) from 1.1.4 (hash: 6ce177c12eeaa6eef68f684846108439) Detected Schema changes: ERROR: error: Error thrown when comparing: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-actions.yaml' does not exist in the specification
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-packages.yaml' does not exist in the specification
    ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [211055:11]
    ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [211057:11]
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-actions.yaml' does not exist in the specification
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-packages.yaml' does not exist in the specification
    ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [210497:11]
    ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [210499:11]

---
+++ clients/GitHubEnterpriseCloud/src/Schema/GlobalAdvisory/Identifiers.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ApiClients\Client\GitHubEnterpriseCloud\Schema\GlobalAdvisory;
+
+use ApiClients\Client\GitHubEnterpriseCloud\Schema;
+
+final readonly class Identifiers extends Schema\AliasAbstract\Tiet649F32EB\TietC13659D6\Tiet0B408A31\TietF85C8E0E
+{
+}
