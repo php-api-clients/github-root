30f56c61a5c9d4622debb22a4600f8177382e820
WyriHaximus <WyriHaximus@users.noreply.github.com>
2023-08-10 13:53:37 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4 (hash: b807e1aa55a31493f276a1c45eb359fa) from 1.1.4 (hash: 8a071f341e7a01c0626fe0ebf3a81441) Detected Schema changes: ERROR: error: Error thrown when comparing: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-actions.yaml' does not exist in the specification
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-packages.yaml' does not exist in the specification
    ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [218124:11]
    ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [218126:11]
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-actions.yaml' does not exist in the specification
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-packages.yaml' does not exist in the specification
    ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [211055:11]
    ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [211057:11]

---
+++ clients/GitHubEnterpriseCloud/src/Schema/WebhookDeploymentReviewRejected/WorkflowJobRun.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ApiClients\Client\GitHubEnterpriseCloud\Schema\WebhookDeploymentReviewRejected;
+
+use ApiClients\Client\GitHubEnterpriseCloud\Schema;
+
+final readonly class WorkflowJobRun extends Schema\AliasAbstract\TietB05C64DE\Tiet4B78C5E2\TietABFD31AC\Tiet7CBAECD2
+{
+}
