9a9f94dbdb17ab5885357a69e5348df6fc8baf3f
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-08-27 18:00:23 +0200

    Another fix for GitHub messing up

--- utils/patch-broken-spec-files.php
+++ utils/patch-broken-spec-files.php
@@ -7,10 +7,16 @@ file_put_contents(
             "      description: 'A group of pull requests that the merge queue has grouped together
         to be merged.
 
-'"
+'",
+            "      description: 'Conditions for an organization ruleset. The conditions object
+        should contain both `repository_name` and `ref_name` properties or both `repository_id`
+        and `ref_name` properties.
+
+'",
         ],
         [
-            "      description: 'A group of pull requests that the merge queue has grouped together to be merged.'"
+            "      description: 'A group of pull requests that the merge queue has grouped together to be merged.'",
+            "      description: 'Conditions for an organization ruleset. The conditions object should contain both `repository_name` and `ref_name` properties or both `repository_id` and `ref_name` properties.'",
         ],
         file_get_contents(
             $argv[1],
