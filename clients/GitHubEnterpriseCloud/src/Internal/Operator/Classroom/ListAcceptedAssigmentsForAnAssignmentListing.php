2e002801654c340734d99b8070ab8936ba1f566c
WyriHaximus <WyriHaximus@users.noreply.github.com>
2023-09-14 15:50:28 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4-82345c77cb5488207ef5eef6763bf7ec from 1.1.4-5b5d2dfb0c53d647bd4e9dd38b133ce5 Detected Schema changes: ERROR: error: Error thrown when comparing: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-actions.yaml' does not exist in the specification
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-packages.yaml' does not exist in the specification
    ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [216966:11]
    ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [216968:11]
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-actions.yaml' does not exist in the specification
    ERROR: local lookups are not permitted, please set AllowFileLookup to true in the configuration
    ERROR: component 'server-statistics-packages.yaml' does not exist in the specification
    ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [216963:11]
    ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [216965:11]

--- clients/GitHubEnterpriseCloud/src/Internal/Operator/Classroom/ListAcceptedAssigmentsForAnAssignmentListing.php
+++ clients/GitHubEnterpriseCloud/src/Internal/Operator/Classroom/ListAcceptedAssigmentsForAnAssignmentListing.php
@@ -24,7 +24,7 @@ final readonly class ListAcceptedAssigmentsForAnAssignmentListing
     {
     }
 
-    /** @return iterable<Schema\ClassroomAcceptedAssignment> */
+    /** @return Observable<Schema\ClassroomAcceptedAssignment> */
     public function call(int $assignmentId, int $page = 1, int $perPage = 30): iterable
     {
         $operation = new \ApiClients\Client\GitHubEnterpriseCloud\Internal\Operation\Classroom\ListAcceptedAssigmentsForAnAssignmentListing($this->responseSchemaValidator, $this->hydrator, $assignmentId, $page, $perPage);
