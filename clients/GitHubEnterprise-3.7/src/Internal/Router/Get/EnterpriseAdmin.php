37276dbc00e5dd143a9072411c275470d381dea1
WyriHaximus <WyriHaximus@users.noreply.github.com>
2023-09-27 13:25:49 +0000

    [GitHubEnterprise-3.7] Update to 1.1.4-459c78815195214803a3332657b9add6 from 1.1.4-217eba5bd4c7f01c411f911fc37b26f6 Detected Schema changes:
    └─┬Paths
      ├─┬/repos/{owner}/{repo}/commits/{ref}/check-suites
      │ └─┬GET
      │   └──[M] description (21983:20)
      ├─┬/repos/{owner}/{repo}/commits/{ref}/check-runs
      │ └─┬GET
      │   └──[M] description (21918:20)
      ├─┬/repos/{owner}/{repo}/check-suites/{check_suite_id}/check-runs
      │ └─┬GET
      │   └──[M] description (20252:20)
      ├─┬/markdown/raw
      │ └──POST
      └─┬/repos/{owner}/{repo}/check-runs/{check_run_id}
        └─┬PATCH
          └──[M] description (19748:20)
    
    Date: 09/27/23 | Commit: New: etc/specs/GitHubEnterprise-3.7/previous.spec.yaml, Original: etc/specs/GitHubEnterprise-3.7/current.spec.yaml
    Document Element | Total Changes | Breaking Changes
    paths            | 4             | 0
    INFO: Total Changes: 4
    INFO: Modifications: 4

--- clients/GitHubEnterprise-3.7/src/Internal/Router/Get/EnterpriseAdmin.php
+++ clients/GitHubEnterprise-3.7/src/Internal/Router/Get/EnterpriseAdmin.php
@@ -206,7 +206,7 @@ final class EnterpriseAdmin
         return $operator->call();
     }
 
-    /** @return Schema\GlobalHook */
+    /** @return */
     public function getGlobalWebhook(array $params): GlobalHook|array
     {
         $arguments = [];
