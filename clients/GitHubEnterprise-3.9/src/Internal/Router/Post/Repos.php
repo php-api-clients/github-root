a042ff80fc40ba6294eed8a89afdd731eaa21584
WyriHaximus <WyriHaximus@users.noreply.github.com>
2024-01-28 10:36:35 +0000

    [GitHubEnterprise-3.9] Update to 1.1.4-6a07c770b153662571e5bc77530deccb from 1.1.4-6a07c770b153662571e5bc77530deccb Detected Schema changes:                                                                                 starting work.                                                                                 Building original model for commit 5a396d
                                                                                    SUCCESS: No changes detected
    
                                                                                    SPEC: extracted 1 commits from history
                                                                                    DONE: completed

--- clients/GitHubEnterprise-3.9/src/Internal/Router/Post/Repos.php
+++ clients/GitHubEnterprise-3.9/src/Internal/Router/Post/Repos.php
@@ -323,6 +323,7 @@ final class Repos
         return $operator->call($arguments['owner'], $arguments['repo']);
     }
 
+    /** @return */
     public function createPagesDeployment(array $params): PageDeployment
     {
         $arguments = [];
