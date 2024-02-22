2fa9487f98bc9297f21dd5870cb3f58383477435
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2024-02-22 17:57:44 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4-23d08a3e03ef26af7beff559f3988932 from 1.1.4-9e2a14ab3b4428c74cab67107599dac3 Detected Schema changes:                                                                                 starting work.                                                                                 Building original model for commit 7c1420 2024-02-22 17:28:34 ERROR unable to open the rolodex file, check specification references and base path                       ├ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory                       └ file: /__w/github-root/github-root/server-statistics-actions.yaml 2024-02-22 17:28:34 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory 2024-02-22 17:28:37 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory 2024-02-22 17:28:37 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory                                                                                 SPEC: extracted 1 commits from history
                                                                                    DONE: completed
    WARNING: warnings reported during processing
    ⚠️  Error thrown when comparing: component 'server-statistics-actions.yaml' does not exist in the specification

--- clients/GitHubEnterpriseCloud/src/PHPStan/ClientCallReturnTypes.php
+++ clients/GitHubEnterpriseCloud/src/PHPStan/ClientCallReturnTypes.php
@@ -1016,19 +1016,19 @@ final readonly class ClientCallReturnTypes implements DynamicMethodReturnTypeExt
         }
 
         if ($call === 'POST /orgs/{org}/copilot/billing/selected_teams') {
-            return $this->typeResolver->resolve('Schema\\Operations\\Copilot\\AddCopilotSeatsForTeams\\Response\\ApplicationJson\\Created|\\ApiClients\\Tools\\OpenApiClient\\Utils\\Response\\WithoutBody');
+            return $this->typeResolver->resolve('');
         }
 
         if ($call === 'DELETE /orgs/{org}/copilot/billing/selected_teams') {
-            return $this->typeResolver->resolve('Schema\\Operations\\Copilot\\CancelCopilotSeatAssignmentForTeams\\Response\\ApplicationJson\\Ok|\\ApiClients\\Tools\\OpenApiClient\\Utils\\Response\\WithoutBody');
+            return $this->typeResolver->resolve('');
         }
 
         if ($call === 'POST /orgs/{org}/copilot/billing/selected_users') {
-            return $this->typeResolver->resolve('Schema\\Operations\\Copilot\\AddCopilotSeatsForUsers\\Response\\ApplicationJson\\Created\\Application\\Json|\\ApiClients\\Tools\\OpenApiClient\\Utils\\Response\\WithoutBody');
+            return $this->typeResolver->resolve('');
         }
 
         if ($call === 'DELETE /orgs/{org}/copilot/billing/selected_users') {
-            return $this->typeResolver->resolve('Schema\\Operations\\Copilot\\CancelCopilotSeatAssignmentForUsers\\Response\\ApplicationJson\\Ok\\Application\\Json|\\ApiClients\\Tools\\OpenApiClient\\Utils\\Response\\WithoutBody');
+            return $this->typeResolver->resolve('');
         }
 
         if ($call === 'GET /orgs/{org}/credential-authorizations') {
@@ -1280,7 +1280,7 @@ final readonly class ClientCallReturnTypes implements DynamicMethodReturnTypeExt
         }
 
         if ($call === 'GET /orgs/{org}/members/{username}/copilot') {
-            return $this->typeResolver->resolve('Schema\\CopilotSeatDetails|\\ApiClients\\Tools\\OpenApiClient\\Utils\\Response\\WithoutBody');
+            return $this->typeResolver->resolve('');
         }
 
         if ($call === 'GET /orgs/{org}/memberships/{username}') {
