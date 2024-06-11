a4723a38c4cb341a61239daa69bd89624372d58a
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2024-06-11 17:55:34 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4-39135bdfcebfe58c32955a3638a37701 from 1.1.4-a520a0da1346bda8d17b35be14a878ac Detected Schema changes:                                                                                 starting work.                                                                                 Building original model for commit 513a33 2024-06-11 17:28:59 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory 2024-06-11 17:28:59 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory 2024-06-11 17:29:01 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory 2024-06-11 17:29:01 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory                                                                                 ERROR: component 'server-statistics-actions.yaml' does not exist in the specification                                                                                 ERROR: component 'server-statistics-packages.yaml' does not exist in the specification                                                                                 ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [204146:11]                                                                                 ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [204148:11]                                                                                 ERROR: component 'server-statistics-packages.yaml' does not exist in the specification                                                                                 ERROR: component 'server-statistics-actions.yaml' does not exist in the specification                                                                                 ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [203498:11]                                                                                 ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [203500:11]

---
+++ clients/GitHubEnterpriseCloud/src/Schema/ExemptionResponse.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ApiClients\Client\GitHubEnterpriseCloud\Schema;
+
+use EventSauce\ObjectHydrator\MapFrom;
+
+final readonly class ExemptionResponse
+{
+    public const SCHEMA_JSON         = '{
+    "title": "Exemption response",
+    "type": "object",
+    "properties": {
+        "id": {
+            "type": "integer",
+            "description": "The ID of the exemption response."
+        },
+        "reviewer_id": {
+            "type": "integer",
+            "description": "The ID of the user who reviewed the exemption request."
+        },
+        "reviewer_login": {
+            "type": "string",
+            "description": "The login of the user who reviewed the exemption request."
+        },
+        "status": {
+            "enum": [
+                "approved",
+                "rejected",
+                "dismissed"
+            ],
+            "type": "string",
+            "description": "The status of the exemption response."
+        },
+        "created_at": {
+            "type": "string",
+            "description": "The date and time the exemption request was created.",
+            "format": "date-time"
+        }
+    },
+    "description": "A response to an exemption request by a delegated bypasser."
+}';
+    public const SCHEMA_TITLE        = 'Exemption response';
+    public const SCHEMA_DESCRIPTION  = 'A response to an exemption request by a delegated bypasser.';
+    public const SCHEMA_EXAMPLE_DATA = '{
+    "id": 2,
+    "reviewer_id": 11,
+    "reviewer_login": "generated",
+    "status": "dismissed",
+    "created_at": "1970-01-01T00:00:00+00:00"
+}';
+
+    /**
+     * id: The ID of the exemption response.
+     * reviewerId: The ID of the user who reviewed the exemption request.
+     * reviewerLogin: The login of the user who reviewed the exemption request.
+     * status: The status of the exemption response.
+     * createdAt: The date and time the exemption request was created.
+     */
+    public function __construct(public int|null $id, #[MapFrom('reviewer_id')]
+    public int|null $reviewerId, #[MapFrom('reviewer_login')]
+    public string|null $reviewerLogin, public string|null $status, #[MapFrom('created_at')]
+    public string|null $createdAt,)
+    {
+    }
+}
