d725695d3b996b964354e93038128edec9597750
Cees-Jan Kiewiet <ceesjank@gmail.com>
2024-09-05 07:52:20 +0200

    Comment out fixed brokages in spec files

--- utils/patch-broken-spec-files.php
+++ utils/patch-broken-spec-files.php
@@ -4,19 +4,19 @@ file_put_contents(
     $argv[1],
     str_replace(
         [
-            "      description: 'A group of pull requests that the merge queue has grouped together
-        to be merged.
-
-'",
-            "      description: 'Conditions for an organization ruleset. The conditions object
-        should contain both `repository_name` and `ref_name` properties or both `repository_id`
-        and `ref_name` properties.
-
-'",
+//            "      description: 'A group of pull requests that the merge queue has grouped together
+//        to be merged.
+//
+//'",
+//            "      description: 'Conditions for an organization ruleset. The conditions object
+//        should contain both `repository_name` and `ref_name` properties or both `repository_id`
+//        and `ref_name` properties.
+//
+//'",
         ],
         [
-            "      description: 'A group of pull requests that the merge queue has grouped together to be merged.'",
-            "      description: 'Conditions for an organization ruleset. The conditions object should contain both `repository_name` and `ref_name` properties or both `repository_id` and `ref_name` properties.'",
+//            "      description: 'A group of pull requests that the merge queue has grouped together to be merged.'",
+//            "      description: 'Conditions for an organization ruleset. The conditions object should contain both `repository_name` and `ref_name` properties or both `repository_id` and `ref_name` properties.'",
         ],
         file_get_contents(
             $argv[1],
