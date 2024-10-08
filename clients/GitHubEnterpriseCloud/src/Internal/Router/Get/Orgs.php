ea212192dd99a4090a6c260054bea8fdfaee60d8
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2024-10-08 17:51:39 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4-b756a9c2ae79f483113462f9d47c6cbf from 1.1.4-61362eae34baa8aaf089660247a67141 Detected Schema changes: 2024-10-08 17:28:50 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory 2024-10-08 17:28:50 ERROR unable to open the rolodex file, check specification references and base path                       ├ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory                       └ file: /__w/github-root/github-root/server-statistics-packages.yaml 2024-10-08 17:28:50 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-advisory-db.yaml                       └ error: open /__w/github-root/github-root/server-statistics-advisory-db.yaml: no such file or directory 2024-10-08 17:28:52 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory 2024-10-08 17:28:52 ERROR unable to open the rolodex file, check specification references and base path                       ├ error: open /__w/github-root/github-root/server-statistics-advisory-db.yaml: no such file or directory                       └ file: /__w/github-root/github-root/server-statistics-advisory-db.yaml 2024-10-08 17:28:52 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory ERROR: component `server-statistics-actions.yaml` does not exist in the specification ERROR: component `server-statistics-packages.yaml` does not exist in the specification ERROR: component `server-statistics-advisory-db.yaml` does not exist in the specification ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [207924:11] ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [207926:11] ERROR: cannot resolve reference `server-statistics-advisory-db.yaml`, it's missing:  [207928:11] ERROR: component `server-statistics-actions.yaml` does not exist in the specification ERROR: component `server-statistics-advisory-db.yaml` does not exist in the specification ERROR: component `server-statistics-packages.yaml` does not exist in the specification ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [207908:11] ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [207910:11] ERROR: cannot resolve reference `server-statistics-advisory-db.yaml`, it's missing:  [207912:11]

--- clients/GitHubEnterpriseCloud/src/Internal/Router/Get/Orgs.php
+++ clients/GitHubEnterpriseCloud/src/Internal/Router/Get/Orgs.php
@@ -1209,6 +1209,7 @@ final class Orgs
         return $operator->call($arguments['org'], $arguments['pat_id'], $arguments['per_page'], $arguments['page']);
     }
 
+    /** @return */
     public function getCustomProperty(array $params): CustomProperty
     {
         $arguments = [];
