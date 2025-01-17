2624d50bf4659978ae7919d5df2436281a8aff7c
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2025-01-17 09:02:30 +0000

    [GitHubEnterprise-3.13] Update to 1.1.4-446a3cd1fdbd8dda94d848b89bd8f6e2 from 1.1.4-398d18348d48e9783363f49ed8fadd49 Detected Schema changes:
    ```
    ├─┬Paths
    │ └─┬/orgs/{org}/memberships/{username}
    │   └─┬PUT
    │     └──[🔀] description (12859:20)
    └─┬Components
      └─┬integration
        └─┬owner
          ├──[➕] anyOf (66758:13)
          ├──[➕] anyOf (65758:7)
          ├──[➖] oneOf (66732:7)❌
          └──[➖] oneOf (65757:7)❌
    
    ```
    
    | Document Element | Total Changes | Breaking Changes |
    |------------------|---------------|------------------|
    | components       | 4             | 2                |
    | paths            | 1             | 0                |
    
    Date: 01/17/25 | Commit: New: etc/specs/GitHubEnterprise-3.13/previous.spec.yaml, Original: etc/specs/GitHubEnterprise-3.13/current.spec.yaml
    
    - ❌ **BREAKING Changes**: _2_ out of _5_
    - **Modifications**: _1_
    - **Removals**: _2_
    - **Additions**: _2_
    - **Breaking Removals**: _2_
    
    ERROR: breaking changes discovered

--- clients/GitHubEnterprise-3.13/src/Schema/Operations/Checks/ListForRef/Response/ApplicationJson/Ok/Application/Json.php
+++ clients/GitHubEnterprise-3.13/src/Schema/Operations/Checks/ListForRef/Response/ApplicationJson/Ok/Application/Json.php
@@ -6,6 +6,6 @@ namespace ApiClients\Client\GitHubEnterprise\Schema\Operations\Checks\ListForRef
 
 use ApiClients\Client\GitHubEnterprise\Schema;
 
-final readonly class Json extends Schema\AliasAbstract\Tiet7C8C7BA3\Tiet97BCC415\TietEB32BDB4\TietC6E8FCD7
+final readonly class Json extends Schema\AliasAbstract\TietD29A14FC\Tiet805269F1\TietA52E66B2\Tiet7F583E73
 {
 }
