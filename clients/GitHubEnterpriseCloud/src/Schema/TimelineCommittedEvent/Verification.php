43479d506d2f0d42faaabed29ffb4ce6286685d6
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2025-01-30 20:57:10 +0000

    [GitHubEnterpriseCloud] Update to 1.1.4-9d96349c2889ab814e17092fa3c3cd46 from 1.1.4-a6db3e8c17ce7749767e24fcd8e8540a Detected Schema changes: 2025-01-30 20:27:50 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory 2025-01-30 20:27:50 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory 2025-01-30 20:27:50 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-advisory-db.yaml                       └ error: open /__w/github-root/github-root/server-statistics-advisory-db.yaml: no such file or directory 2025-01-30 20:27:53 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-actions.yaml                       └ error: open /__w/github-root/github-root/server-statistics-actions.yaml: no such file or directory 2025-01-30 20:27:53 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-packages.yaml                       └ error: open /__w/github-root/github-root/server-statistics-packages.yaml: no such file or directory 2025-01-30 20:27:53 ERROR unable to open the rolodex file, check specification references and base path                       ├ file: /__w/github-root/github-root/server-statistics-advisory-db.yaml                       └ error: open /__w/github-root/github-root/server-statistics-advisory-db.yaml: no such file or directory ERROR: component `server-statistics-actions.yaml` does not exist in the specification ERROR: component `server-statistics-packages.yaml` does not exist in the specification ERROR: component `server-statistics-advisory-db.yaml` does not exist in the specification ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [215019:11] ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [215021:11] ERROR: cannot resolve reference `server-statistics-advisory-db.yaml`, it's missing:  [215023:11] ERROR: component `server-statistics-actions.yaml` does not exist in the specification ERROR: component `server-statistics-packages.yaml` does not exist in the specification ERROR: component `server-statistics-advisory-db.yaml` does not exist in the specification ERROR: cannot resolve reference `server-statistics-actions.yaml`, it's missing:  [214891:11] ERROR: cannot resolve reference `server-statistics-packages.yaml`, it's missing:  [214893:11] ERROR: cannot resolve reference `server-statistics-advisory-db.yaml`, it's missing:  [214895:11]

--- clients/GitHubEnterpriseCloud/src/Schema/TimelineCommittedEvent/Verification.php
+++ clients/GitHubEnterpriseCloud/src/Schema/TimelineCommittedEvent/Verification.php
@@ -6,6 +6,6 @@ namespace ApiClients\Client\GitHubEnterpriseCloud\Schema\TimelineCommittedEvent;
 
 use ApiClients\Client\GitHubEnterpriseCloud\Schema;
 
-final readonly class Verification extends Schema\AliasAbstract\TietE746E247\Tiet29FCA330\Tiet8C6E229F\TietB9C53521
+final readonly class Verification extends Schema\AliasAbstract\TietADA0376A\TietE291E6D1\Tiet2A83A4E8\TietFA266CAF
 {
 }
