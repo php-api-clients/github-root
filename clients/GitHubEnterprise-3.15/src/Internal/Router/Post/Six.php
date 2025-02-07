4b10893c9ef1cdfb5851a1e23468af594a8f6af0
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2025-02-07 10:57:00 +0000

    [GitHubEnterprise-3.15] Update to 1.1.4-8e70edec105746983b03daa8864a8df8 from 1.1.4-882141f1b59adcc27a5ddb1aac77be7c Detected Schema changes:
    ```
    └─┬Paths
      ├─┬/scim/v2/enterprises/{enterprise}/Groups
      │ └─┬GET
      │   └─┬Parameters
      │     └──[🔀] description (41193:22)
      └─┬/scim/v2/enterprises/{enterprise}/Users
        └─┬GET
          └─┬Parameters
            └──[🔀] description (41545:22)
    
    ```
    
    | Document Element | Total Changes | Breaking Changes |
    |------------------|---------------|------------------|
    | paths            | 2             | 0                |
    
    Date: 02/07/25 | Commit: New: etc/specs/GitHubEnterprise-3.15/previous.spec.yaml, Original: etc/specs/GitHubEnterprise-3.15/current.spec.yaml
    
    - **Total Changes**: _2_
    - **Modifications**: _2_

--- clients/GitHubEnterprise-3.15/src/Internal/Router/Post/Six.php
+++ clients/GitHubEnterprise-3.15/src/Internal/Router/Post/Six.php
@@ -35,7 +35,6 @@ final class Six
     {
     }
 
-    /** @return |Schema\Operations\Actions\GenerateRunnerJitconfigForEnterprise\Response\ApplicationJson\Created */
     public function call(string $call, array $params, array $pathChunks): Created|Json|\ApiClients\Client\GitHubEnterprise\Schema\Operations\Apps\RedeliverWebhookDelivery\Response\ApplicationJson\Accepted\Application\Json|\ApiClients\Client\GitHubEnterprise\Schema\Operations\Actions\GenerateRunnerJitconfigForEnterprise\Response\ApplicationJson\Created|AuthenticationToken|WithoutBody|TeamDiscussion|\ApiClients\Client\GitHubEnterprise\Schema\Operations\Projects\MoveCard\Response\ApplicationJson\Created\Application\Json|EmptyObject|CodeScanningSarifsReceipt|\ApiClients\Client\GitHubEnterprise\Schema\Operations\DependencyGraph\CreateRepositorySnapshot\Response\ApplicationJson\Created|ShortBlob|GitCommit|GitRef|GitTag|GitTree|PageBuildStatus|PageDeployment|ReleaseNotesContent|SecretScanningPushProtectionBypass|Status|TagProtection|GroupResponse|UserResponse|TeamDiscussionComment|Reaction
     {
         if ($pathChunks[0] === '') {
