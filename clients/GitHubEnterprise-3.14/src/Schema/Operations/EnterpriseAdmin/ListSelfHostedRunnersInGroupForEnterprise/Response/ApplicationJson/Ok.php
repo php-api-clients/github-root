264f7d2eb084469d4df06172f22a161a8c12a1d1
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2025-03-14 09:55:03 +0000

    [GitHubEnterprise-3.14] Update to 1.1.4-d38aae92477e79392ec7b388fbc01e51 from 1.1.4-b1c711111d09784b5c8eea125320b49b Detected Schema changes:
    ```
    └─┬Components
      └─┬runner
        ├─┬id
        │ └──[🔀] description (69576:24)
        └─┬runner_group_id
          └──[🔀] description (69581:24)
    
    ```
    
    | Document Element | Total Changes | Breaking Changes |
    |------------------|---------------|------------------|
    | components       | 2             | 0                |
    
    Date: 03/14/25 | Commit: Original: etc/specs/GitHubEnterprise-3.14/current.spec.yaml, Modified: etc/specs/GitHubEnterprise-3.14/previous.spec.yaml,
    
    - **Total Changes**: _2_
    - **Modifications**: _2_

--- clients/GitHubEnterprise-3.14/src/Schema/Operations/EnterpriseAdmin/ListSelfHostedRunnersInGroupForEnterprise/Response/ApplicationJson/Ok.php
+++ clients/GitHubEnterprise-3.14/src/Schema/Operations/EnterpriseAdmin/ListSelfHostedRunnersInGroupForEnterprise/Response/ApplicationJson/Ok.php
@@ -6,6 +6,6 @@ namespace ApiClients\Client\GitHubEnterprise\Schema\Operations\EnterpriseAdmin\L
 
 use ApiClients\Client\GitHubEnterprise\Schema;
 
-final readonly class Ok extends Schema\AliasAbstract\TietED690D6D\Tiet788B85F7\Tiet90BDCBBD\Tiet6547B867
+final readonly class Ok extends Schema\AliasAbstract\TietF71405B1\Tiet858A5095\Tiet77E03F50\TietEFE21D34
 {
 }
